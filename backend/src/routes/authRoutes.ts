b0608d6d89110d494c459d05d88504ad5f24726c
Ozan797 <ozan8@hotmail.co.uk>
2024-09-26 13:19:12 +0100

    Added GET request for profile retrieval

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, getUserProfile } from "../controllers/authController";
+import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+// Protected Routes
+router.get("/me", protect, getUserProfile)
 
 export default router;
